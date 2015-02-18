b14aa8a07996685c2a69cd0a4ad00d7400268ca3
GrilloPress <grillopress@gmail.com>
2015-02-18 22:58:22 +0000

    create ex3.c to learn string manipulation

---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 10;
+	int height = 72;
+	
+	printf("I used to be %d year's old \n" , age);
+	printf("I have no idea what %d height is... maybe cm? \n", height);
+	
+	return 0;
+}

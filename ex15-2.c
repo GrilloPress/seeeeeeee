fbb1a8712b0a7b71608ffbfeabf45d3fdb823195
GrilloPress <grillopress@gmail.com>
2015-02-22 17:22:35 +0000

    add first pointer example

--- ex15-2.c
+++ ex15-2.c
@@ -3,15 +3,22 @@
 int main(int argc, char *argv[])
 {
 	
-	// an array. Loops through ages and names.
+	// a pointer version
 	int ages[] = {3, 25, 28};
 	char *names[] = {"Alfred", "Lydia", "Andrew"};
 	
+	// define 
 	int count = sizeof(ages) / sizeof(int);
 	int i = 0;
 	
-	for(i = 0; i < count; i++){
-		printf("%s, %d \n", names[i], ages[i]);
+	// defines the pointers
+	int *cur_ages = ages;
+	char **cur_names = names;
+	
+	for(i = 0; i < count; i++) {
+		
+		printf("%s: %d \n", *(cur_names+i), *(cur_ages+i));
+		
 	}
 	
 	return 0;

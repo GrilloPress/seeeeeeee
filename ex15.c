9708b5deb5a1d0ac422dfb93bffbfcbc61519baf
GrilloPress <grillopress@gmail.com>
2015-02-22 16:44:23 +0000

    add array age loop

--- ex15.c
+++ ex15.c
@@ -2,8 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+	int ages[] = {3, 25, 28};
+	char *names[] = {"Alfred", "Lydia", "Andrew"};
 	
+	int count = sizeof(ages) / sizeof(int);
+	int i = 0;
 	
+	for(i = 0; i < count; i++){
+		printf("%s, %d \n", names[i], ages[i]);
+	}
 	
 	return 0;
 }
